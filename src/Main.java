17e886706a8edec9c7e297b195adf952c8913053
JekBodr <jekbodr@gmail.com>
2021-11-21 11:04:37 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Мой Git Проект");
         System.out.println("Мой Super Проект");
+        System.out.println("Мой Git Проект");
+        System.out.println("Мой Super Проект");
     }
 }
